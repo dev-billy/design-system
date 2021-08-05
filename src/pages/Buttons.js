383c7d0ef5f4e3ea33e5b79164d8cbbe87cbe72f
Billy <billypatrick767@gmail.com>
2021-08-05 11:45:49 +0300

    added buttons listing basic setup

--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -1,12 +1,58 @@
 import Layout from "../layout";
 import Button from "../components/Button";
+import styles from "../styles/buttonspage.module.scss";
 function ButtonDisplayPage() {
   return (
     <Layout>
-      <h1>Different Variations of Buttons here</h1>
-      <Button color="primary" />
-      <Button variant="text" color="danger" />
-      <Button variant="outlined" color="primary" disableShadow disabled />
+      <h1>Buttons</h1>
+      <div className={styles.all}>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button />"} </code>
+            <Button />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"&:hover, &:focus"} at me to see effect </code>
+            <Button />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='outline' />"} </code>
+            <Button variant="outlined" color="primary" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"&:hover, &:focus"} at me to see effect </code>
+            <Button variant="outlined" color="primary" />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='text' />"} </code>
+            <Button variant="text" color="primary" disableShadow />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"&:hover, &:focus"} at me to see effect </code>
+            <Button variant="text" color="primary" disableShadow />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button disableShadow />"} </code>
+            <Button variant="default" color="primary" disableShadow />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button disabled />"} </code>
+            <Button disabled />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='text' disabled/>"} </code>
+            <Button variant="text" disabled />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
