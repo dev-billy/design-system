87a8d33bd7bfd80c3ace19cab18c77b0f7e2aded
Billy <billypatrick767@gmail.com>
2021-08-05 15:32:47 +0300

    added the different variations of buttons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,11 @@ import { AddShoppingCartRounded as AddCart } from "@material-ui/icons";
 function Icon(props) {
   const { name, style } = props;
   return (
-    <>{name === "add_cart" && <AddCart fontSize="small" style={style} />}</>
+    <>
+      {name === "local_grocery_store" && (
+        <AddCart fontSize="small" style={style} />
+      )}
+    </>
   );
 }
 
--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -11,35 +11,31 @@ function ButtonDisplayPage() {
             <code>{"<Button />"} </code>
             <Button />
           </div>
-          <div className={styles.displayBtn}>
-            <code>{"&:hover, &:focus"} at me to see effect </code>
-            <Button />
-          </div>
         </div>
         <div className={styles.buttonGroup}>
           <div className={styles.displayBtn}>
-            <code>{"<Button variant='outline' />"} </code>
-            <Button variant="outlined" color="primary" />
-          </div>
-          <div className={styles.displayBtn}>
-            <code>{"&:hover, &:focus"} at me to see effect </code>
+            <code>{"<Button variant='outlined' color='primary'/>"} </code>
             <Button variant="outlined" color="primary" />
           </div>
         </div>
         <div className={styles.buttonGroup}>
           <div className={styles.displayBtn}>
-            <code>{"<Button variant='text' />"} </code>
-            <Button variant="text" color="primary" disableShadow />
-          </div>
-          <div className={styles.displayBtn}>
-            <code>{"&:hover, &:focus"} at me to see effect </code>
+            <code>{"<Button variant='text' color='primary'/>"} </code>
             <Button variant="text" color="primary" disableShadow />
           </div>
         </div>
         <div className={styles.buttonGroup}>
           <div className={styles.displayBtn}>
-            <code>{"<Button disableShadow />"} </code>
-            <Button variant="default" color="primary" disableShadow />
+            <code>{"<Button color='primary' disableShadow />"} </code>
+            <Button color="primary" disableShadow />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='outlined' disableShadow />"} </code>
+            <Button variant="outlined" color="primary" disableShadow />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='text'  disableShadow={false} />"}</code>
+            <Button variant="text" color="primary" disableShadow={false} />
           </div>
         </div>
         <div className={styles.buttonGroup}>
@@ -50,6 +46,60 @@ function ButtonDisplayPage() {
           <div className={styles.displayBtn}>
             <code>{"<Button variant='text' disabled/>"} </code>
             <Button variant="text" disabled />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button variant='outlined' disabled/>"} </code>
+            <Button variant="outlined" disabled />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button startIcon='local_grocery_store' />"} </code>
+            <Button color="primary" startIcon="local_grocery_store" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button endIcon='local_grocery_store' />"} </code>
+            <Button color="primary" endIcon="local_grocery_store" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button endIcon='local_grocery_store' />"} </code>
+            <Button
+              color="primary"
+              endIcon="local_grocery_store"
+              variant="outlined"
+            />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button size='sm' />"} </code>
+            <Button size="sm" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button size='md' />"} </code>
+            <Button size="md" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button size='lg' />"} </code>
+            <Button size="lg" />
+          </div>
+        </div>
+        <div className={styles.buttonGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Button color='default' />"} </code>
+            <Button color="default" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button color='primary' />"} </code>
+            <Button color="primary" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button color='secondary' />"} </code>
+            <Button color="secondary" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Button color='danger' />"} </code>
+            <Button color="danger" />
           </div>
         </div>
       </div>
