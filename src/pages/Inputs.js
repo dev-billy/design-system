645ca2fb4927ec2f1c698c509b029726808b6722
Billy <billypatrick767@gmail.com>
2021-08-07 19:36:42 +0300

    added different variants of inputs to the page

--- src/pages/Inputs.js
+++ src/pages/Inputs.js
@@ -1,16 +1,83 @@
 import Input from "../components/Input";
 import Layout from "../layout";
-
+import styles from "../styles/inputspage.module.scss";
 function Inputs() {
   return (
     <Layout>
       <h1>Inputs</h1>
-      <Input />
-      <Input error />
-      <Input helperText="some interesting text" error />
-      <Input helperText="some interesting text" />
-      <Input endIcon="password" />
-      <Input startIcon="phone" />
+      <div className={styles.all}>
+        <h3>All Inputs</h3>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input />"} </code>
+            <Input />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input error/>"} </code>
+            <Input error />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input disabled />"} </code>
+            <Input disabled />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input helperText='Some interesting text' />"} </code>
+            <Input helperText="Some interesting text" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input helperText='Some interesting text' />"} </code>
+            <Input helperText="Some interesting text" error />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input startIcon='phone' />"} </code>
+            <Input startIcon="phone" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input endIcon='password' />"} </code>
+            <Input endIcon="password" />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input size='sm' />"} </code>
+            <Input size="sm" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input size='md' />"} </code>
+            <Input size="md" />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input size='lg' />"} </code>
+            <Input size="lg" />
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <div className={styles.displayBtn}>
+            <code>{"<Input   />"} </code>
+            <Input />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input   />"} </code>
+            <Input />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input   />"} </code>
+            <Input />
+          </div>
+          <div className={styles.displayBtn}>
+            <code>{"<Input   />"} </code>
+            <Input />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
